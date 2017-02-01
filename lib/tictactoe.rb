6cf66859d461a33d12201e7e05f8002d042654c5
Sam Joseph <tansaku@gmail.com>
2017-02-01 18:07:39 +0000

    split interactions into two and read table layout from file

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,5 +1,12 @@
 class TicTacToe
-  def self.play
+  def self.choose_player
     puts 'Which player do you want to be? X or O?'
+    STDIN.gets
+  end
+  def self.choose_move  
+    puts File.read('./spec/fixtures/first_move_result.txt')
+    puts
+    puts 'Where do you want to move?'
+    STDIN.gets
   end
 end
--- spec/features/gameplay_spec.rb
+++ spec/features/gameplay_spec.rb
@@ -1,9 +1,18 @@
 require 'tictactoe'
 
 describe 'gameplay' do
+  subject(:play_game) {  }
 
   it 'should ask which player you want to be' do
-    expect { TicTacToe.play }.to output("Which player do you want to be? X or O?\n").to_stdout
+    expect(STDIN).to receive(:gets).and_return 'O'
+    expect { TicTacToe.choose_player }.to output("Which player do you want to be? X or O?\n").to_stdout  
   end
+
+
+  it 'should ask what move you want to make' do
+    expect(STDIN).to receive(:gets).and_return 'B2'
+    first_move = File.read('./spec/fixtures/first_move_result.txt')
+    expect { TicTacToe.choose_move }.to output("#{first_move}\n\nWhere do you want to move?\n").to_stdout
+  end  
 
 end
